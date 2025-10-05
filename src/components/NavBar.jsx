a7a1a3a915c3434cb57361d4f7155106c3b5026e
Marco <marko.barraza@gmail.com>
2025-10-04 23:42:24 -0300

    redirecion en navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     const setActiveClass = ({ isActive }) => `${style.btnNav} ${isActive ? style.active : ''}`
   return (
     <div className={style.contenedorNavbar}>
-      <NavLink className={ setActiveClass } to='/'>Cuponera</NavLink>
+      <NavLink className={ setActiveClass } to='/cuponera'>Cuponera</NavLink>
       <NavLink className={ setActiveClass } to='/contadores'>Contadores</NavLink>
     </div>
   )
